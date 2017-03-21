443831a762a0987965c166bb729f4f97d9f0b2a9
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2017-03-21 17:39:20 +0100

    bugfixing

--- bin/merge_featurecounts.py
+++ bin/merge_featurecounts.py
@@ -9,33 +9,35 @@ from collections import defaultdict
 def do_the_thing(dest_dir,out_file,input_files):
    table_dict=defaultdict(dict)
 
+   import pdb 
+   pdb.set_trace() 
 
-     while input_files:
-        for input_file in input_files:
-            sample_name=os.path.basename(fastq_file)
-            sample_name.replace("Aligned.sortedByCoord.out_gene.featureCounts.txt","")
-            with open(input_file, 'r') as f:
-                if not line.startswith('ENSG')
-                    next(f)
-                for line in f:
-                    
-                    gene=
-                    gene_count=
-                    table_dict[sample_name]=dict()
-                    table_dict[sample_name][gene]=gene_count 
+   for input_file in input_files:
+       sample_name=os.path.basename(input_file)
+       sample_name= sample_name.replace("Aligned.sortedByCoord.out_gene.featureCounts.txt","")
+       table_dict[sample_name]=dict()
+       with open(input_file, 'r') as f:
+           for line in f:
+               if not line.startswith('E'):
+                   continue
+               line_info=line.split('\t')
+               gene=line_info[0]
+               gene_count=line_info[-1:]
+               table_dict[sample_name][gene]=gene_count 
     
     
 
 
 
 
- if __name__ == "__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="""Merges the counts for all the samples in a project
     """)
     parser.add_argument("dest_dir", metavar='Output directory', nargs='?', default='.',
-                                   help="Path to output. ")
-    parser.add_argument("out_file" metavar ="Name of output file", nargs '?', default="all_counts.txt",
-                                   help= "Name of the output file that will be created"
-    parser.add_argument("input_files", metavar='Input file', nargs='+', default='*.featureCounts.txt',
+                                   help="Path to output.")
+    parser.add_argument("out_file", metavar ='Name of output file', nargs='?', default='all_counts.txt',
+                                   help= "Name of the output file that will be created")
+    parser.add_argument("input_files", metavar='Input file', nargs=argparse.REMAINDER, default='*.featureCounts.txt',
                                    help="Path to the outputfiles from FeatureCounts. ")
-    args = parser.parse_args() 
+    args = parser.parse_args()
+    do_the_thing(args.dest_dir, args.out_file, args.input_files)
