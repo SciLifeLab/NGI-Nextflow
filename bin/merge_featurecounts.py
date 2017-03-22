af2d7af8c0c669afe6df3e09ff2724d55fedacb4
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2017-03-22 16:42:07 +0100

    issues with argparse

--- bin/merge_featurecounts.py
+++ bin/merge_featurecounts.py
@@ -9,13 +9,27 @@ from collections import defaultdict
 def do_the_thing(dest_dir,out_file,input_files):
    table_dict=defaultdict(dict)
 
-   import pdb 
-   pdb.set_trace() 
-
+   gene_list=[]
+   sample_names=[]
    for input_file in input_files:
+       if not gene_list:
+           first_file=True
+       print"strated on {}".format(input_file)
        sample_name=os.path.basename(input_file)
+       sample_names.append(sample_name)
        sample_name= sample_name.replace("Aligned.sortedByCoord.out_gene.featureCounts.txt","")
        table_dict[sample_name]=dict()
+     
+       #Extract the gene list from the first file
+       if first_file:
+           with open(input_file, 'r') as f:
+               for line in f:
+                   if not line.startswith('E'):
+                       continue
+                   line_info=line.split('\t')
+                   gene=line_info[0]
+                   gene_list.append(gene)
+
        with open(input_file, 'r') as f:
            for line in f:
                if not line.startswith('E'):
@@ -23,12 +37,21 @@ def do_the_thing(dest_dir,out_file,input_files):
                line_info=line.split('\t')
                gene=line_info[0]
                gene_count=line_info[-1:]
-               table_dict[sample_name][gene]=gene_count 
-    
-    
+               table_dict[sample_name][gene]=gene_count
+            #   for ensamble_id in gene_list:
+             #      if ensamble_id == gene:
+              #         table_dict[gene][sample_name]=gene_count 
+       #                print "Gene is {} and sample_name {}".format(gene,sample_name)    
+   import pdb 
+   pdb.set_trace() 
 
-
-
+       #printing stuff
+   with open(out_file, 'w') as f:
+       for gene in gene_list:
+           f.write(gene)
+           for sample_name in sample_names:
+               f.write('\t'+table_dict[gene][sample_name])
+           f.write('\n')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="""Merges the counts for all the samples in a project
